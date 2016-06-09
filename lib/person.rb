1d8f3eb174a280d4ae45e22765fe95f9899a4d10
chaplonglau <chaplonglau@gmail.com>
2016-06-09 08:52:38 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person 
+  def name
+    @name
+  end
+
+  def name=(get_name)
+    @name=get_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(get_job)
+    @job=get_job
+  end
+  
+
+end
